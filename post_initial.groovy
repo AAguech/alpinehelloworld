a3eddf48cc7ae73ba48106ab3dff3f9e6ecfb9f2
AAguech <110027044+AAguech@users.noreply.github.com>
2022-08-05 12:40:43 +0200

    Update post_initial.groovy

--- post_initial.groovy
+++ post_initial.groovy
@@ -1,6 +1,6 @@
   post {
        success {
-         slackSend (color: '#00FF00', message: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+         slackSend (color: '#00FF00', message: "Amira:SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
          }
       failure {
             slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
